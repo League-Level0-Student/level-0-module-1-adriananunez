9967a7216df5894464b4260a1fbca8708cdbf24a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-16 10:57:09 -0700

    lol

--- src/Unbirthday.java
+++ src/Unbirthday.java
@@ -2,9 +2,12 @@ import javax.swing.JOptionPane;
 
 public class Unbirthday {
 public static void main(String[] args) {
-	JOptionPane.showInputDialog(null, "What is your birthday?");
-	
-	
-	
+	String birthday= JOptionPane.showInputDialog(null, "What is your birthday?");
+	if(birthday.equals("6-16")) {
+	System.out.println("Happy Birthday");
+	}
+	else {
+		System.out.println("Merry Unbirthday");
+	}
 }
 }
